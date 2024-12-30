9f09bda6de1cece8afd1464fde9489e3804448a1
suckm <suckm@DESKTOP-DUMRNUO>
2024-12-30 14:38:48 +0900

    q2741

--- baekjoon/src/q2741/Main.java
+++ baekjoon/src/q2741/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int N = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
             System.out.println(i);
         }
 	}
