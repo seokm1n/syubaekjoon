5306867d3563d834135ca2f05d3b4c58cc566a6e
suckm <suckm@DESKTOP-DUMRNUO>
2025-01-06 12:11:10 +0900

    q2588

---
+++ baekjoon/src/q2588/Main.java
@@ -0,0 +1,26 @@
+package q2588;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+
+		int b1 = b/100;
+		int b2 = b%100/10;
+		int b3 = b%10;
+		int result1 = a*b3;
+		int result2 = a*b2;
+		int result3 = a*b1;
+		int result = a*b;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result);
+	}
+}
