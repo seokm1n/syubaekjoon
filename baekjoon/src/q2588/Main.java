8ef391a4dfe9177fcfc37cd1a3f0400d86bb3dd5
suckm <suckm@DESKTOP-DUMRNUO>
2025-01-06 14:14:39 +0900

    q2588

--- baekjoon/src/q2588/Main.java
+++ baekjoon/src/q2588/Main.java
@@ -10,18 +10,9 @@ public class Main {
 		int b = sc.nextInt();
 		sc.close();
 
-		int b1 = b/100;
-		int b2 = b%100/10;
-		int b3 = b%10;
-		
-		int result1 = a*b3;
-		int result2 = a*b2;
-		int result3 = a*b1;
-		int result = a*b;
-		
-		System.out.println(result1);
-		System.out.println(result2);
-		System.out.println(result3);
-		System.out.println(result);
+		System.out.println(a * (b % 10));
+		System.out.println(a * (b % 100 / 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
 	}
 }
