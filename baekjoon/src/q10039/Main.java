4ad4cef60cce4b8c07616496754bf0b35a9e2736
suckm <suckm@DESKTOP-DUMRNUO>
2025-01-09 14:45:06 +0900

    10039

--- baekjoon/src/q10039/Main.java
+++ baekjoon/src/q10039/Main.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		int[] score = new int[5];
-		int sum = 0;
+		int avg = 0;
 		Scanner sc = new Scanner(System.in);
 		for (int i = 0; i < score.length; i++) {
 			score[i] = sc.nextInt();
 			if (score[i] < 40) {
 				score[i] = 40;
 			}
-			sum += score[i];
+			avg += score[i]/score.length;
 		}
 		sc.close();
-		System.out.println(sum/score.length);
+		System.out.println(avg);
 	}
 
 }
