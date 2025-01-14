2610acc5e315afc4ac97f098f83f1d26d31bb5d4
suckm <suckm@DESKTOP-DUMRNUO>
2025-01-14 17:17:16 +0900

    8958

--- baekjoon/src/q8958/Main.java
+++ baekjoon/src/q8958/Main.java
@@ -7,19 +7,19 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		String[] str = new String[N];
-		
+
 		for (int i = 0; i < str.length; i++) {
 			str[i] = sc.next();
 		}
 		sc.close();
-		
+
 		for (int i = 0; i < N; i++) {
 			int score = 0;
 			int sum = 0;
 			for (int j = 0; j < str[i].length(); j++) {
-				
+
 				if (str[i].charAt(j) == 'O') {
-					sum += ++score;					
+					sum += ++score;
 				} else {
 					score = 0;
 				}
