6dab7e395ccfaafda184bf662083f94458ec9496
suckm <suckm@DESKTOP-DUMRNUO>
2025-01-21 14:28:34 +0900

    3052

--- baekjoon/src/q3052/Main.java
+++ baekjoon/src/q3052/Main.java
@@ -3,12 +3,22 @@ package q3052;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[10];
-		for (int i = 0; i < 10; i++) {
-			arr[i] = sc.nextInt();
-			System.out.printf("%d",(arr[i] % 42));
-		}		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt() % 42;
+        }
+        sc.close();
+
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            int j;
+            for (j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) break;
+            }
+            if (j == i) count++;
+        }
+        System.out.println(count);
+    }
 }
