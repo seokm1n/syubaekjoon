07a9a5cf5e663a468d0090404f092703f58e233c
suckm <suckm@DESKTOP-DUMRNUO>
2025-02-18 09:40:01 +0900

    .

---
+++ baekjoon/src/q3046/Main.java
@@ -0,0 +1,13 @@
+package q3046;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		System.out.print(2 * b - a);
+	}
+}
