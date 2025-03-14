9e74fa2147f4f215baef882fb4e4c54edbc80af8
suckm <suckminzzz@naver.com>
2025-03-14 09:11:25 +0900

    .

---
+++ baekjoon/src/q4949/Main.java
@@ -0,0 +1,49 @@
+package q4949;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> strList = new ArrayList<>();
+
+		while (true) {
+			String str = sc.nextLine();
+			if (str.equals(".")) {
+				break;
+			}
+			strList.add(str);
+		}
+		for (int i = 0; i < strList.size(); i++) {
+			if (check(strList.get(i))) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+		}
+		sc.close();
+	}
+
+	public static boolean check(String str) {
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if (ch == '(' || ch == '[') {
+				stack.push(ch);
+			} else if (ch == ')') {
+				if (stack.isEmpty() || stack.pop() != '(') {
+					return false;
+				}
+			} else if (ch == ']') {
+				if (stack.isEmpty() || stack.pop() != '[') {
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+}
